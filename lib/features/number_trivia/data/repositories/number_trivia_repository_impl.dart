11b3c1bca8cf3ecba5de8579bac53d2ab03864bf
Karan Ramani <karan.ramani@gmail.com>
2020-01-28 22:36:09 +0530

    Chapter 6 - implementing repository...

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -8,7 +8,6 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
-
   final NumberTriviaRemoteDataSource remoteDataSource;
   final NumberTriviaLocalDataSource localDataSource;
   final NetworkInfo networkInfo;
@@ -20,14 +19,16 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   });
 
   @override
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
-    // TODO: implement getConcreteNumberTrivia
-    return null;
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    networkInfo.isConnected;
+    final remoteTrivia = await remoteDataSource.getConcreteNumberTrivia(number);
+    await localDataSource.cacheNumberTrivia(remoteTrivia);
+    return Right(remoteTrivia);
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
-    // TODO: implement getRandomNumberTrivia
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
     return null;
   }
 }
--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,13 +1,19 @@
 import 'package:catdd_numbertrivia/core/platform/network_info.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:catdd_numbertrivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
-import 'package:catdd_numbertrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:catdd_numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class MockNumberTriviaRemoteDataSource extends Mock implements NumberTriviaRemoteDataSource {}
-class MockNumberTriviaLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+class MockNumberTriviaRemoteDataSource extends Mock
+    implements NumberTriviaRemoteDataSource {}
+
+class MockNumberTriviaLocalDataSource extends Mock
+    implements NumberTriviaLocalDataSource {}
+
 class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
@@ -16,17 +22,64 @@ void main() {
   MockNetworkInfo mockNetworkInfo;
   NumberTriviaRepositoryImpl numberTriviaRepositoryImpl;
 
-  mockNumberTriviaRemoteDataSource = MockNumberTriviaRemoteDataSource();
-  mockNumberTriviaLocalDataSource = MockNumberTriviaLocalDataSource();
-  mockNetworkInfo = MockNetworkInfo();
+  setUp(() {
+    mockNumberTriviaRemoteDataSource = MockNumberTriviaRemoteDataSource();
+    mockNumberTriviaLocalDataSource = MockNumberTriviaLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    numberTriviaRepositoryImpl = NumberTriviaRepositoryImpl(
+      remoteDataSource: mockNumberTriviaRemoteDataSource,
+      localDataSource: mockNumberTriviaLocalDataSource,
+      networkInfo: mockNetworkInfo,
+    );
+  });
 
-  numberTriviaRepositoryImpl = NumberTriviaRepositoryImpl(
-    remoteDataSource: mockNumberTriviaRemoteDataSource,
-    localDataSource: mockNumberTriviaLocalDataSource,
-    networkInfo: mockNetworkInfo,
-  );
+  group('getConcreteNumberTrivia', () {
+    // DATA FOR THE MOCKS AND ASSERTIONS
+    // We'll use these three variables throughout all the tests
+    final tNumber = 1;
+    final tNumberTriviaModel =
+    NumberTriviaModel(number: tNumber, trivia: 'test trivia');
+    final NumberTrivia tNumberTrivia = tNumberTriviaModel;
 
-  test('NumberTriviaRepositoryImpl should implement NumberTriviaRepository', (){
-    expect(numberTriviaRepositoryImpl, isA<NumberTriviaRepository>());
+    test('should check if the device is online', () {
+      //arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      // act
+      numberTriviaRepositoryImpl.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockNetworkInfo.isConnected);
+    });
+
+    group('device is online', () {
+      test(
+          'Should get entity NumberTrivia with data when remoteDataSoure call is successful',
+              () async {
+            //arrange
+            when(mockNumberTriviaRemoteDataSource.getConcreteNumberTrivia(
+                tNumber))
+                .thenAnswer((_) async => tNumberTriviaModel);
+            //act
+            final result = await
+            numberTriviaRepositoryImpl.getConcreteNumberTrivia(tNumber);
+            //assert
+            verify(
+                mockNumberTriviaRemoteDataSource.getConcreteNumberTrivia(
+                    tNumber));
+            expect(result, equals(Right(tNumberTrivia)));
+          });
+
+      test(
+          'Should cache the data locally when remoteDataSoure call is successful', () async {
+        //arrange
+        when(mockNumberTriviaRemoteDataSource.getConcreteNumberTrivia(tNumber))
+            .thenAnswer((_) async => tNumberTriviaModel);
+        //act
+        await numberTriviaRepositoryImpl.getConcreteNumberTrivia(tNumber);
+        //assert
+        verify(numberTriviaRepositoryImpl.getConcreteNumberTrivia(tNumber));
+        verify(
+            mockNumberTriviaLocalDataSource.cacheNumberTrivia(tNumberTrivia));
+      });
+    });
   });
 }
