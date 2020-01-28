b0ef869c9c1609094859a44952d23626b94f25d6
Karan Ramani <karan.ramani@gmail.com>
2020-01-28 10:32:17 +0530

    Chapter 5 - Respository Impl..

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,9 +1,24 @@
 import 'package:catdd_numbertrivia/core/error/failures.dart';
+import 'package:catdd_numbertrivia/core/platform/network_info.dart';
+import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    @required this.remoteDataSource,
+    @required this.localDataSource,
+    @required this.networkInfo,
+  });
+
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
     // TODO: implement getConcreteNumberTrivia
--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -2,6 +2,8 @@ import 'package:catdd_numbertrivia/core/platform/network_info.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:catdd_numbertrivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:catdd_numbertrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class MockNumberTriviaRemoteDataSource extends Mock implements NumberTriviaRemoteDataSource {}
@@ -17,9 +19,14 @@ void main() {
   mockNumberTriviaRemoteDataSource = MockNumberTriviaRemoteDataSource();
   mockNumberTriviaLocalDataSource = MockNumberTriviaLocalDataSource();
   mockNetworkInfo = MockNetworkInfo();
+
   numberTriviaRepositoryImpl = NumberTriviaRepositoryImpl(
-      remoteDataSource: mockNumberTriviaRemoteDataSource,
-      localDataSource: mockNumberTriviaLocalDataSource,
-      netowkrInfo: mockNetworkInfo,
+    remoteDataSource: mockNumberTriviaRemoteDataSource,
+    localDataSource: mockNumberTriviaLocalDataSource,
+    networkInfo: mockNetworkInfo,
   );
+
+  test('NumberTriviaRepositoryImpl should implement NumberTriviaRepository', (){
+    expect(numberTriviaRepositoryImpl, isA<NumberTriviaRepository>());
+  });
 }
